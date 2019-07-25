1b1d4dd2cac2284447160c390f2428b3f2aeafb2
nik frank <nikbfrank@gmail.com>
2019-07-25 02:08:40 -0700

    animated table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import rawFiles from './files';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const convertFileDates = files => files.map((file, i)=> ({
+  ...file,
+  updated: (new Date(file.updated)).toString().slice(0,24),
+}) );
+
+const files = convertFileDates(rawFiles.map((f, i)=> ({
+  ...f, position: i,
+})) );
+
+class App extends React.Component {
+  state = {
+    files,
+    currentSort: null,
+  }
+
+  sortBy = field => ()=> {
+    if( this.state.currentSort === field+'-up'){
+      const sortedFiles = JSON.parse(JSON.stringify(files))
+      .sort((fileA, fileB)=>
+        fileA[field] > fileB[field] ? -1 : 1
+      );
+
+      this.setState({
+        currentSort: field+'-dn',
+        files: convertFileDates(rawFiles.map(file=> ({
+          ...file,
+          position: sortedFiles.findIndex(f=> file.name === f.name),
+        })))
+      });
+
+    } else {
+      const sortedFiles = JSON.parse(JSON.stringify(files))
+      .sort((fileA, fileB)=>
+        fileA[field] > fileB[field] ? 1 : -1
+      );
+
+      this.setState({
+        currentSort: field+'-up',
+        files: convertFileDates(rawFiles.map(file=> ({
+          ...file,
+          position: sortedFiles.findIndex(f => file.name === f.name),
+        })) )
+      });
+    }
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='sorting-table'>
+          <div className='header-row'>
+            <div>
+              <button onClick={this.sortBy('name')}>Name</button>
+            </div>
+            <div>
+              <button onClick={this.sortBy('size')}>Size</button>
+            </div>
+            <div>
+              <button onClick={this.sortBy('updated')}>Updated</button>
+            </div>
+          </div>
+          {
+            this.state.files.map((file, i)=> (
+              <div className='row'
+                   key={file.name}
+                   style={{ top: ((file.position+1) * 100/ (files.length + 1))+'%' }}>
+                <div>{file.name}</div>
+                <div>{file.size}</div>
+                <div>{file.updated}</div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/files.js
@@ -0,0 +1,11 @@
+export default [
+  { name: 'data.json', size: 3096, updated: Date.now() -100061300 },
+  { name: 'dan.mp4', size: 3496, updated: Date.now() -200000300 },
+  { name: 'thursday.bmp', size: 13096, updated: Date.now() -20077700 },
+  { name: 'cat-pictures.tiff', size: 6066, updated: Date.now() -140082200 },
+  { name: '.gitignore', size: 175, updated: Date.now() -40000500 },
+  { name: 'src/App.js', size: 91084, updated: Date.now() -230006700 },
+  { name: 'server/index.js', size: 1488, updated: Date.now() -1008700 },
+  { name: 'police-report.pdf', size: 819924, updated: Date.now() -180001200 },
+  { name: 'michael-jackson-home-video.avi', size: 12345, updated: Date.now() -190003400 },
+]
